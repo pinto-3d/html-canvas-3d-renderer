8245d098c71af203438f25a4fdbf3899776e8079
pinto-3d <pinto.3d@outlook.com>
2026-09-16 22:10:37 -0500

    added mtl file parsing to add colors from files

--- src/FileImport3D.tsx
+++ src/FileImport3D.tsx
@@ -1,8 +1,73 @@
 import { ColorRGBA } from "./2D"
 import { Face, Mesh, Object3D, Vector3 } from "./3D"
 
+class Mtl {
+    name: string = ""
+    Ka: ColorRGBA = ColorRGBA.hotPink
+    Kd: ColorRGBA = ColorRGBA.hotPink
+    Ks: ColorRGBA = ColorRGBA.hotPink
+    Ke: ColorRGBA = ColorRGBA.hotPink
+    Ni: number = 0
+    Ns: number = 0
+    d: number = 0
+    illum: number = 0
+}
+
 class FileImport3D{
-    static OBJ_Import(fileContent: string, color: ColorRGBA){
+
+    static OBJ_Import(fileContent: string, mtlContent: string, color: ColorRGBA){
+
+
+        let curMtl: Mtl|null = null;
+        let mtlMap: Map<String, Mtl> = new Map();
+
+        
+        let list: string[] = []
+        if(mtlContent.includes("\r\n")){
+            list = mtlContent.split("\r\n")
+        }
+        else{
+            list = mtlContent.split("\n")
+        }
+
+        for(let i=0;i<list.length;i++){
+            let args: string[] = list[i].split(" ")
+            switch(args[0]){
+                case "newmtl":
+                    if(curMtl){
+                        mtlMap.set(curMtl.name, structuredClone(curMtl));
+                    }
+                    curMtl = new Mtl();
+                    curMtl.name = args[1];
+                    break;
+                case "Ns":
+                    curMtl!.Ns = Number.parseFloat(args[1]);
+                    break;
+                case "Ni":
+                    curMtl!.Ni = Number.parseFloat(args[1]);
+                    break;
+                case "d":
+                    curMtl!.d = Number.parseFloat(args[1]);
+                    break;
+                case "illum":
+                    curMtl!.illum = Number.parseInt(args[1]);
+                    break;
+                case "Ka":
+                    curMtl!.Ka = new ColorRGBA(Number.parseFloat(args[1])*255, Number.parseFloat(args[2])*255, Number.parseFloat(args[3])*255);
+                    break;
+                case "Kd":
+                    curMtl!.Kd = new ColorRGBA(Number.parseFloat(args[1])*255, Number.parseFloat(args[2])*255, Number.parseFloat(args[3])*255);
+                    break;
+                case "Ks":
+                    curMtl!.Ks = new ColorRGBA(Number.parseFloat(args[1])*255, Number.parseFloat(args[2])*255, Number.parseFloat(args[3])*255);
+                    break;
+                case "Ke":
+                    curMtl!.Ke = new ColorRGBA(Number.parseFloat(args[1])*255, Number.parseFloat(args[2])*255, Number.parseFloat(args[3])*255);
+                    break;
+            }
+        }
+
+
         let objName: string = ""
         let objs: Object3D[] = []
         let verts: Vector3[] = []
@@ -11,7 +76,7 @@ class FileImport3D{
         let vertNormals: Vector3[] = []
         let currentColor: ColorRGBA = ColorRGBA.hotPink
 
-        let list: string[] = []
+        list = []
         if(fileContent.includes("\r\n")){
             list = fileContent.split("\r\n")
         }
@@ -61,8 +126,8 @@ class FileImport3D{
                     break
                 case "usemtl":
                     let curMatName: string = args[1]
-                    if(ColorRGBA.matColors.has(curMatName)){
-                        currentColor = ColorRGBA.fromColorRGBA(ColorRGBA.matColors.get(curMatName)!)
+                    if(mtlMap.has(curMatName)){
+                        currentColor = ColorRGBA.fromColorRGBA(mtlMap.get(curMatName)?.Kd!)
                     }
                     else{
                         let temp = 0
@@ -81,8 +146,18 @@ class FileImport3D{
     
     static async ImportIsland(){
         const response = await fetch('/3d renderer island.obj');
-        const islandContent = await response.text();
-        return FileImport3D.OBJ_Import(islandContent, ColorRGBA.hotPink)
+        const content = await response.text();
+        const mtlresponse = await fetch('/3d renderer island.mtl');
+        const mtlcontent = await mtlresponse.text();
+        return FileImport3D.OBJ_Import(content, mtlcontent, ColorRGBA.hotPink)
+    }
+    
+    static async ImportMario(){
+        const response = await fetch('/mario.obj');
+        const marioContent = await response.text();
+        const mtlResponse = await fetch('/mario.mtl');
+        const mtlMarioContent = await mtlResponse.text();
+        return FileImport3D.OBJ_Import(marioContent, mtlMarioContent, ColorRGBA.hotPink)
     }
 
     static async Image_Import(filePath: string, sx: number = 0, sy: number = 0, sWidth: number = -1, sHeight: number = -1){
